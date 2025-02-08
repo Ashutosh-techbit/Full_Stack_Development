057cffadc580b9b9c5576f58437eeb6536c30bc9
Ashutosh bansal <bansalashutosh77@gmail.com>
2025-02-08 13:16:47 +0530

    using sql in cli

---
+++ 37_NodejsWithSQL/index.js
@@ -0,0 +1,32 @@
+const { faker } = require('@faker-js/faker');
+const mysql = require("mysql2");
+
+const connection =  mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password : 'ashu2005@'
+});
+
+
+try{connection.query("SHOW TABLES",(err,result)=>{
+  if(err) throw err;
+  console.log(result); 
+});
+}
+catch{
+console.log(err);
+}
+
+connection.end(); //to end connection after execution
+
+let getRandomUser = ()=> {
+  return {
+    id: faker.string.uuid(),
+    username: faker.internet.username(), // before version 9.1.0, use userName()
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+}
+
+// console.log(getRandomUser());
---
+++ 37_NodejsWithSQL/schema.sql
@@ -0,0 +1,3 @@
+-- source C:/Users/atulb/OneDrive/Desktop/Full_Stack_Development/37_NodejsWithSQL/schema.sql;
+
+SHOW TABLES;
