1c26eb96eadb9b88273e3be5e7c613786e4190d9
Ashutosh bansal <bansalashutosh77@gmail.com>
2025-02-09 19:22:00 +0530

    user updation

--- 37_NodejsWithSQL/index.js
+++ 37_NodejsWithSQL/index.js
@@ -1,33 +1,33 @@
-const { faker, da } = require('@faker-js/faker');
+const { faker, da } = require("@faker-js/faker");
 const mysql = require("mysql2");
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 
-
-const connection =  mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'delta_app',
-  password : 'ashu2005@'
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "delta_app",
+  password: "ashu2005@",
 });
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"/views"));
-
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 //getrandomUser in key:pair method
-let getRandomUser = ()=> {
+let getRandomUser = () => {
   return {
     id: faker.string.uuid(),
     username: faker.internet.username(), // before version 9.1.0, use userName()
     email: faker.internet.email(),
     password: faker.internet.password(),
   };
-}
+};
 
 //arrow fucntion contains return a array to create fake data
-
 // let getRandomUser = ()=> {
 //   return [
 //      faker.string.uuid(),
@@ -37,82 +37,111 @@ let getRandomUser = ()=> {
 //   ];
 // };
 
-
 // // let q = "SHOW TABLES";
 // let q = "INSERT INTO user(id , name , email , password) VALUES ?";
 // // let user = [["124","harsh","harsh@gmail.com","hi!3"],["125","yash","yash@gmail.com","hlo234"]];
-
-
 // let data=[]; //empty object
-
 // for(let i =1;i<=100;i++){
 //   data.push(getRandomUser()); //push 100 fake user data into empty data object
 // }
-
-
-
-
 // connection.end(); //to end connection after execution
 
-//home route
 
-app.get("/",(req,res)=>{
-  let q = "SELECT count(*) FROM user"
-  try{
-      connection.query(q,(err,result)=>{
-      if(err) throw err;
+app.get("/", (req, res) => {
+  let q = "SELECT count(*) FROM user";
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
       let count = result[0]["count(*)"];
-      res.render("home.ejs",{count})
-      console.log(result); 
+      res.render("home.ejs", { count });
+      console.log(result);
       // res.send("success")
     });
-    }
-    catch{
+  } catch {
     console.log(err);
-    res.send("error in database")
-    }
+    res.send("error in database");
+  }
 
   console.log("welcome to HomePage");
-})
+});
 
 //show all user route
-app.get("/user",(req,res)=>{
-  let q = "SELECT * FROM user"
-  try{
-      connection.query(q,(err,users)=>{
-      if(err) throw err;
+app.get("/user", (req, res) => {
+  let q = "SELECT * FROM user";
+  try {
+    connection.query(q, (err, users) => {
+      if (err) throw err;
       console.log(users[0]);
-      res.render("showusers.ejs",{users});
-
+      res.render("showusers.ejs", { users });
     });
-    }
-    catch{
+  } catch {
     console.log(err);
-    res.send("error in database")
-    }
-})
+    res.send("error in database");
+  }
+});
 
 //edit user
-app.get("/user/:id/edit",(req,res)=>{
-  let {id} = req.params;
-  
-  res.render("edit.ejs",{id})
-})
+app.get("/user/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let q = `SELECT * FROM user WHERE id='${id}'`;
 
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+      console.log(user);
+      res.render("edit.ejs", { user });
+    });
+  } catch {
+    console.log(err);
+    res.send("error in database");
+  }
+});
 
-app.listen("8080",()=>{
-  console.log("server is listening to port 8080")
-})
-  
+app.patch("/user/:id", (req, res) => {
+  let { id } = req.params;
+  let { password: formPass, username: newUsername } = req.body;
+
+  //get user by id
+  let q = `SELECT * FROM user where id='${id}'`;
+
+  try {
+    connection.query(q, (err, result) => {
+
+      if (err) throw err;
+
+      let user = result[0];
+      if (formPass !== user.password) {
+        res.send("WRONG PASSWORD");
+      } else {
+        let q2 = `UPDATE user SET name='${newUsername}' WHERE id='${id}'`;
+        connection.query(q2, (err, result) => {
+          if (err) throw err;
+          res.redirect("/user");
+        });
+      }
+      // console.log(user);
+      // res.render("edit.ejs", { user });
+    });
+  } catch {
+    console.log(err);
+    res.send("error in database");
+  }
+  // res.send(updated)
+});
+
+app.listen("8080", () => {
+  console.log("server is listening to port 8080");
+});
 
 // //result is in array form can traverse , length()
-  // // try{connection.query(q,[user],(err,result)=>{
-  // try{
-  //   connection.query(q,[data],(err,result)=>{
-  //   if(err) throw err;
-  //   console.log(result); 
-  // });
-  // }
-  // catch{
-  // console.log(err);
-  // }
+// // try{connection.query(q,[user],(err,result)=>{
+// try{
+//   connection.query(q,[data],(err,result)=>{
+//   if(err) throw err;
+//   console.log(result);
+// });
+// }
+// catch{
+// console.log(err);
+// }
