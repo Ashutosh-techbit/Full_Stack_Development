f8449b04f8d5b5b8b5740f2464471e3634d74a51
Ashutosh bansal <bansalashutosh77@gmail.com>
2025-02-13 15:31:29 +0530

    update

--- 40_MongoWithExpress/index.js
+++ 40_MongoWithExpress/index.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
+
 const path = require("path");
 
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"))
 
-main().catch(err=>console.log(err));
-
-async function main(params) {
-    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp")
-}
 
 
 app.get("/",(req,res)=>{
