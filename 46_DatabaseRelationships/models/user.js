de241eb6e54cc168514f3a1ad1f9f3d79ab9883e
Ashutosh bansal <bansalashutosh77@gmail.com>
2025-02-22 23:47:12 +0530

    database relationship

---
+++ 46_DatabaseRelationships/models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+main().then(()=>console.log("connected successfully")).catch(err=>console.log(err))
+
+async function main(params) {
+      await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo")    
+}
+
+const userSchema = new Schema({
+    username: String,
+    address : [
+        {
+            location : String,
+            city : String,
+        }
+    ]
+
+});
+
+const User = mongoose.model("User",userSchema);
+const addUsers = async()=>{
+    let user1 = new User({ 
+        username : "Elon",
+         address:[
+            {_id : false ,location : "US",city:"vegas"}
+            
+        ],
+
+    });
+        user1.address.push({location:"india",city:"bihar"});
+        let result = await user1.save();
+        console.log(result)
+}
+
+addUsers();
