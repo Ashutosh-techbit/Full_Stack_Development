2f089aabae88cbca175907a490ec90894185185d
Ashutosh bansal <bansalashutosh77@gmail.com>
2025-03-22 19:42:07 +0530

    sessions

--- Cookie/server.js
+++ Cookie/server.js
@@ -3,39 +3,62 @@ const app = express();
 const users = require("./routes/users")
 const posts = require("./routes/posts")
 const cookieparser = require("cookie-parser")
+const session  = require("express-session")
 
-app.use(cookieparser("secret"));
+// ================ Session =============================
 
-app.get("/getsignedcookies",(req,res)=>{
-    res.cookie("color","red",{signed:true});
-    res.send("signed cookie sent")
+app.use(session({secret:"mysupersecretstring",resave:false , saveUninitialized:true}));
+
+app.get("/test",(req,res)=>{
+    res.send("test done!!")
 })
 
-app.get("/verify",(req,res)=>{
-    console.log(req.signedCookies);
-    res.send("verified");
-})
-
-app.get("/getcookies",(req,res)=>{
-    res.cookie("name","ashu");
-    res.cookie("age","19");
-    res.send("Check your baked cookies :)");
-})
-
-app.get("/",(req,res)=>{
-    console.log(req.cookies);
-    res.send("Hi i am root");
+//req.session.count take count of number of requests - ye count same browser pr tab open krne se count add honge naaki 1 se start honge 
+// this stores in temproary memnory
+app.get("/response",(req,res)=>{
+    if(req.session.count){
+        req.session.count++;
+    }else{
+        req.session.count=1;
+    }
+    res.send(`you send ${req.session.count} requests`);
 })
 
 
+// ==========COOKIES=====================
 
-app.get("/greet",(req,res)=>{
-    let {name = "anonymous"} = req.cookies;
-    res.send(`hi ${name}`)
-})
+// app.use(cookieparser("secret"));
 
-app.use("/users",users);
-app.use("/posts",posts);
+// app.get("/getsignedcookies",(req,res)=>{
+//     res.cookie("color","red",{signed:true});
+//     res.send("signed cookie sent")
+// })
+
+// app.get("/verify",(req,res)=>{
+//     console.log(req.signedCookies);
+//     res.send("verified");
+// })
+
+// app.get("/getcookies",(req,res)=>{
+//     res.cookie("name","ashu");
+//     res.cookie("age","19");
+//     res.send("Check your baked cookies :)");
+// })
+
+// app.get("/",(req,res)=>{
+//     console.log(req.cookies);
+//     res.send("Hi i am root");
+// })
+
+
+
+// app.get("/greet",(req,res)=>{
+//     let {name = "anonymous"} = req.cookies;
+//     res.send(`hi ${name}`)
+// })
+
+// app.use("/users",users);
+// app.use("/posts",posts);
 
 app.listen(3030,()=>{
     console.log("server started at 3030");
